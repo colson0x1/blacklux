7a420aa1c9718394eb7e7dc8d259087b5b31f665
Colson <colsoncalifornication@gmail.com>
2022-08-29 14:27:25 +0545

    created slug, added individual query to retrieve product, made the slug dynamic using next router

---
+++ frontend/pages/product/[slug].js
@@ -0,0 +1,36 @@
+import { useQuery } from 'urql';
+import { GET_PRODUCT_QUERY } from '../../lib/query';
+import { useRouter } from 'next/router';
+
+export default function ProductDetails() {
+  // fetch slug
+  const { query } = useRouter();
+
+  // fetch GraphQL
+  const [results] = useQuery({
+    query: GET_PRODUCT_QUERY,
+    variables: { slug: query.slug },
+  });
+  const { data, fetching, error } = results;
+
+  // check for incoming data
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oops... {error.message}</p>;
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <h3>Title</h3>
+        <p>Description</p>
+      </div>
+      <div>
+        <span>Quantity</span>
+        <button>Plus</button>
+        <p>0</p>
+        <button>Minus</button>
+      </div>
+      <button>Add to cart</button>
+    </div>
+  );
+}
