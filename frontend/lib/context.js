b46a44a771dd8948d5cd722d50ebb8445d5db782
Colson <colsoncalifornication@gmail.com>
2022-08-30 22:21:51 +0545

    created, styled and added states to cart component

--- frontend/lib/context.js
+++ frontend/lib/context.js
@@ -35,7 +35,25 @@ export const StateContext = ({ children }) => {
         )
       );
     } else {
-        setCartItems([...cartItems, {...product, quantity: quantity }]);
+      setCartItems([...cartItems, { ...product, quantity: quantity }]);
+    }
+  };
+
+  // remove product
+  const onRemove = (product) => {
+    // check if product is already in the cart
+    const exist = cartItems.find((item) => item.slug === product.slug);
+    
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.slug !== product.slug));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.slug === product.slug
+            ? { ...exist, quantity: exist.quantity - 1 }
+            : item
+        )
+      );
     }
   };
 
@@ -49,6 +67,7 @@ export const StateContext = ({ children }) => {
         setShowCart,
         cartItems,
         onAdd,
+        onRemove,
       }}
     >
       {children}
