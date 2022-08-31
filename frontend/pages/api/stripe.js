a7782d7981e8a3035681f18a8414c3d2b54195cc
Colson <colsoncalifornication@gmail.com>
2022-09-01 01:16:09 +0545

    implemented feature that helps users to purchase without an account

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -6,49 +6,92 @@ const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 export default async function handler(req, res) {
   const session = getSession(req, res);
   const user = session?.user;
-  const stripeId = user['https://localhost:3000/stripe_customer_id'];
-  console.log(user);
+  if (user) {
+    const stripeId = user['https://localhost:3000/stripe_customer_id'];
 
-  if (req.method === 'POST') {
-    try {
-      // start checkout session
-      const session = await stripe.checkout.sessions.create({
-        submit_type: 'pay',
-        mode: 'payment',
-        customer: stripeId,
-        payment_method_types: ['card'],
-        shipping_address_collection: {
-          allowed_countries: ['US', 'CA', 'GB', 'DE'],
-        },
-        allow_promotion_codes: true,
-        shipping_options: [
-          { shipping_rate: 'shr_1LckpUGHQEpK0iNIesNj9NHi' },
-          { shipping_rate: 'shr_1LclKTGHQEpK0iNIxK8jW3uJ' },
-        ],
-        line_items: req.body.map((item) => {
-          return {
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: item.title,
-                images: [item.image.data.attributes.formats.thumbnail.url],
+    if (req.method === 'POST') {
+      try {
+        // start checkout session
+        const session = await stripe.checkout.sessions.create({
+          submit_type: 'pay',
+          mode: 'payment',
+          customer: stripeId,
+          payment_method_types: ['card'],
+          shipping_address_collection: {
+            allowed_countries: ['US', 'CA', 'GB', 'DE'],
+          },
+          allow_promotion_codes: true,
+          shipping_options: [
+            { shipping_rate: 'shr_1LckpUGHQEpK0iNIesNj9NHi' },
+            { shipping_rate: 'shr_1LclKTGHQEpK0iNIxK8jW3uJ' },
+          ],
+          line_items: req.body.map((item) => {
+            return {
+              price_data: {
+                currency: 'usd',
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
               },
-              unit_amount: item.price * 100,
-            },
-            adjustable_quantity: {
-              enabled: true,
-              minimum: 1,
-            },
-            quantity: item.quantity,
-          };
-        }),
-        // takes to the success/failed page
-        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/canceled`,
-      });
-      res.status(200).json(session);
-    } catch (error) {
-      res.status(error.statusCode || 500).json(error.message);
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          // takes to the success/failed page
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (error) {
+        res.status(error.statusCode || 500).json(error.message);
+      }
+    }
+  } else {
+    if (req.method === 'POST') {
+      try {
+        // start checkout session
+        const session = await stripe.checkout.sessions.create({
+          submit_type: 'pay',
+          mode: 'payment',
+          payment_method_types: ['card'],
+          shipping_address_collection: {
+            allowed_countries: ['US', 'CA', 'GB', 'DE'],
+          },
+          allow_promotion_codes: true,
+          shipping_options: [
+            { shipping_rate: 'shr_1LckpUGHQEpK0iNIesNj9NHi' },
+            { shipping_rate: 'shr_1LclKTGHQEpK0iNIxK8jW3uJ' },
+          ],
+          line_items: req.body.map((item) => {
+            return {
+              price_data: {
+                currency: 'usd',
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
+              },
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          // takes to the success/failed page
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (error) {
+        res.status(error.statusCode || 500).json(error.message);
+      }
     }
   }
 }
