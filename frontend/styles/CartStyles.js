7982a5bca4670852b14971ef9a11e981f4480a61
Colson <colsoncalifornication@gmail.com>
2022-08-31 10:51:51 +0545

    used framer motion and animated cart

--- frontend/styles/CartStyles.js
+++ frontend/styles/CartStyles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+// animation
+const {motion} = require('framer-motion');
 
-export const CartWrapper = styled.div`
+export const CartWrapper = styled(motion.div)`
   position: fixed;
   right: 0;
   top: 0;
@@ -12,7 +14,7 @@ export const CartWrapper = styled.div`
   justify-content: flex-end;
 `;
 
-export const CartStyle = styled.div`
+export const CartStyle = styled(motion.div)`
   width: 40%;
   background: #f1f1f1;
   padding: 2rem 5rem;
@@ -20,7 +22,7 @@ export const CartStyle = styled.div`
   position: relative;
 `;
 
-export const Card = styled.div`
+export const Card = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -35,7 +37,7 @@ export const Card = styled.div`
   }
 `;
 
-export const CardInfo = styled.div`
+export const CardInfo = styled(motion.div)`
   width: 50%;
   div {
     display: flex;
@@ -43,10 +45,9 @@ export const CardInfo = styled.div`
   }
 `;
 
-export const EmptyStyle = styled.div`
+export const EmptyStyle = styled(motion.div)`
   position: absolute;
   top: 0;
-  left: 50%;
   transform: translate(-50%, 0%);
   display: flex;
   flex-direction: column;
@@ -67,7 +68,7 @@ export const EmptyStyle = styled.div`
   }
 `;
 
-export const Checkout = styled.div`
+export const Checkout = styled(motion.div)`
   button {
     background: var(--primary);
     padding: 1rem 2rem;
@@ -78,3 +79,5 @@ export const Checkout = styled.div`
     border: none;
   }
 `;
+
+export const Cards = styled(motion.div)``;
