8ef4e24f46e88d8e9bfa82c5afa1c4a87655c4a1
Colson <colsoncalifornication@gmail.com>
2022-09-01 11:22:58 +0545

    version 1.0.0

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -15,7 +15,7 @@ export default function Product({ product }) {
       <Link href={`/product/${slug}`}>
         <h2>{title}</h2>
       </Link>
-      <h3>{price}</h3>
+      <h3>${price}</h3>
     </ProductStyle>
   );
 }
